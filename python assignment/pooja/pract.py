2b2caf6d0e4783e654776abb4af1e50f31fc9465
Shivansh <aman@crossml.com>
2021-08-13 18:08:45 +0530

    made changes in assignment, django url, and models

--- python assignment/pooja/pract.py
+++ python assignment/pooja/pract.py
@@ -4,68 +4,76 @@ from datetime import datetime
 import getpass
 import os.path
 import json
+import random
+
+# Access system time
 time = datetime.now().time().strftime('%H:%M:%S')
 print(f"Time: {time}")
-# This will access the current user
 
+# access user data
 username = getpass.getuser()
 print("Username: "+ username)
-print()
 
 # will access the home directory
-parent_dir = os.path.expanduser("~")
+parent_dir = os.path.expanduser("~print(")
 files_loc = parent_dir+'/users/'
-# print(parent_dir)
-print(files_loc)
-
-# import pathlib
-# it will check the current working directory's path
-# print(pathlib.Path.cwd()) #error while concating value.
 
 folder_path = os.getcwd()+'/users/'
 # print(folder_path)
 all_files = os.listdir(os.getcwd()+'/users/')
-print(all_files)
-# creating a list of files.
+print()
 
-users_name=[]
-for files in all_files:
-    file_data = open(folder_path+files, "r")
-    for names in file_data:
-        users_name.append(names.strip())
-    file_data.close()
 
-# print(users_name)
-import random
-def uniq_uuid(users):
-    # n = random.randint(1,9000000000)
-    # print(hex(n))
-    # b=hex(n)
-    for user in users:
-        b=random.randint(1000,9000000000)
-        # print(user.split())
-        name = user.split()
-        # print(user)
-        # print(hex(b)+name[0])
-    return hex(b)+name[0]
 
-# print(uniq_uuid(users_name))
-uniq_uuid(users_name)
+class DataInfo:
+    users_name=[]
+    def read_data(self, folderpath, allfiles):
+        self.folderpath = folderpath
+        self.allfiles = allfiles
+        users_name=[]
+        for files in allfiles:
+            # print(files)
+            file_data = open(self.folderpath+files, "r")
+            for names in file_data:
+                users_name.append(names.strip())
 
-def generate_email(users):
-    for user in users:
+            file_data.close()
+        return users_name
+
+
+    def generate_uuid(self, users):
+        self.users = users
+        # print(users)
+        for user in users:
+            b=random.randint(1000,9000000000)
+            # print(user.split())
+            name = user.split()
+
+        return hex(b)+name[0]
+
+# Generate email for every user
+    def generate_email(self, user):
+        self.user = user
+        print(user)
         first_name = user.split()[0]
         last_name = user.split()[1]
+        # print(first_name +' '+last_name)
+
         email = first_name +"_"+ last_name[0]+ "@virtual.com"
-    return email
+        return email
 
-print(generate_email(users_name))
-user_data =[]
-for user_name in users_name:
-    user_data.append({uniq_uuid(users_name):{"username":user_name,"email":generate_email(users_name)}})
+# convert data into json
+    def data_convert_into_json(self,users):
+        self.users = users
+        user_data = []
+        for user_name in users:
+            user_data.append({self.generate_uuid(user_name):{"username":user_name,"email":self.generate_email(user_name)}})
 
-print(json.dumps(user_data))
+        return json.dumps(user_data)
 
-json_output = open("json_output.txt","w")
-json_output.writelines(str(user_data))
+mydata = DataInfo()
+users_list = mydata.read_data(folder_path,all_files)
+output= mydata.data_convert_into_json(users_list)
+json_output = open("json_output.json","w")
+json_output.writelines(str(output))
 json_output.close()
